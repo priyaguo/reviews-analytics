7c272cfab40b809f39356cdd6a9a854630b28efd
Priya Guo <priya.guo@gmail.com>
2021-05-06 15:42:50 +0800

    first verson

---
+++ read.py
@@ -0,0 +1,14 @@
+data = []
+count = 0 
+with open('reviews.txt', 'r') as f:
+    for line in f:
+        data.append(line)
+        count += 1
+        # % 餘數
+        if count % 10000 == 0:
+            print(len(data))
+
+print(len(data))
+print(data[0])
+print('-------------')
+print(data[1])
