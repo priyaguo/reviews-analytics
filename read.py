f2690f5b337e181ef1bdcf40b5754606a070afa9
Priya Guo <priya.guo@gmail.com>
2021-05-06 15:58:33 +0800

    third version

--- read.py
+++ read.py
@@ -5,16 +5,28 @@ with open('reviews.txt', 'r') as f:
         data.append(line)
         count += 1
         # % 餘數
-        if count % 10000 == 0:
-            print(len(data))
+        if count % 100000 == 0:
+            print('讀取檔案進度第',len(data),'筆')
 print('檔案讀取完了,總共有',len(data),'筆資料')
-
+print('----------------------')
+#計算平均留言長度
 sum_len = 0
 avg_len = 0
 for d in data:
     sum_len += len(d)
 avg_len = sum_len/count
 print('平均留言長度是',avg_len)
+print('----------------------')
+
+#找出長度小於一百
+new = []
+for d in data:
+    if len(d) < 100:
+        new.append(d)
+print('一共有',len(new),'筆留言長度小於一百')
+print('第一筆留言資料:',new[0])
+print('第二筆留言資料:',new[1])
+print('----------------------')
 
 
 """
